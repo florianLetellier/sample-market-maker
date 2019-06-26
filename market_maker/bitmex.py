dde728db73c6d97c55fb72d9e51ec5b34bb20c94
Florian <florian@kryptalgo.com>
2019-06-26 14:58:31 +0200

    asdf

--- market_maker/bitmex.py
+++ market_maker/bitmex.py
@@ -235,7 +235,7 @@ class BitMEX(object):
         # or you could change the clOrdID (set {"clOrdID": "new", "origClOrdID": "old"}) so that an amend
         # can't erroneously be applied twice.
         if max_retries is None:
-            max_retries = 0 if verb in ['POST', 'PUT'] else 3
+            max_retries = 10 if verb in ['POST', 'PUT'] else 30
 
         # Auth: API Key/Secret
         auth = APIKeyAuthWithExpires(self.apiKey, self.apiSecret)
