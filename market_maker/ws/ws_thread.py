a24eeb1f37112ebf94fcd7de921d9f32062d0165
Florian <florian@kryptalgo.com>
2019-06-26 16:52:30 +0200

    asdf

--- market_maker/ws/ws_thread.py
+++ market_maker/ws/ws_thread.py
@@ -172,7 +172,7 @@ class BitMEXWebsocket():
         self.logger.info("Started thread")
 
         # Wait for connect before continuing
-        conn_timeout = 5
+        conn_timeout = 50
         while (not self.ws.sock or not self.ws.sock.connected) and conn_timeout and not self._error:
             sleep(1)
             conn_timeout -= 1
